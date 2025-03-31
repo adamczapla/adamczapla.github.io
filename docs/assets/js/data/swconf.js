47634ab67312c783818e07f586915ab5e8523b6f
Adam Czapla <adam@adamczapla.de>
2025-03-31 12:39:18 +0200

    Deploy statisch nach docs für GitHub Pages

---
+++ docs/assets/js/data/swconf.js
@@ -0,0 +1,28 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1743417500',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
