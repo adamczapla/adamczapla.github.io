0eab6e15f73887f539bc192cf454bb480ec9307e
Adam Czapla <adam@adamczapla.de>
2025-04-04 16:07:19 +0200

    rename posts url - replace ++ with cpp

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743774309',resources: [
+    cacheName: 'chirpy-1743775377',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
